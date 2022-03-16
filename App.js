4011f53fd3a7ce4cadcccfa959977ac292702709
Mbugua Caleb <mbuguacaleb30@gmail.com>
2022-03-16 07:13:50 +0300

    introducing-flat-list

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
 import { StatusBar } from 'expo-status-bar'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, FlatList, StyleSheet, Text } from 'react-native'
+import CategoryItem from './src/components/CategoryItem'
 import Header from './src/components/Header'
 import Search from './src/components/Search'
 
 export default function App() {
+  const commonCategories = [
+    {
+      name: 'Burger',
+      imageUrl: require('./src/assets/images/burger.png'),
+    },
+    {
+      name: 'Pizza',
+      imageUrl: require('./src/assets/images/pizza.png'),
+    },
+    {
+      name: 'Dessert',
+      imageUrl: require('./src/assets/images/cake.png'),
+    },
+    {
+      name: 'Pasta',
+      imageUrl: require('./src/assets/images/pasta.png'),
+    },
+    {
+      name: 'Drinks',
+      imageUrl: require('./src/assets/images/smoothies.png'),
+    },
+    {
+      name: 'Steak',
+      imageUrl: require('./src/assets/images/steak.png'),
+    },
+  ]
   return (
     <View style={styles.container}>
       <Header />
       <Search />
+      <FlatList
+        data={commonCategories}
+        renderItem={({ item, index }) => {
+          return (
+            <CategoryItem
+              name={item.name}
+              imageUrl={item.imageUrl}
+              index={index}
+            />
+          )
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(category) => {
+          category.name
+        }}
+      />
+
       <StatusBar />
     </View>
   )
---
+++ src/components/CategoryItem.js
@@ -0,0 +1,49 @@
+import { View, Text, StyleSheet, Image } from 'react-native'
+import { elevation } from '../common/styles'
+
+export default function CategoryItem({ name, imageUrl, index }) {
+  return (
+    <View
+      style={[
+        styles.container,
+        styles.elevation,
+        index === 0 ? { marginLeft: 25 } : { marginLeft: 15 },
+      ]}
+    >
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={imageUrl} />
+      </View>
+      <Text style={styles.header}>{name}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 70,
+    height: 100,
+    borderRadius: 50,
+    marginVertical: 15,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  elevation,
+  image: {
+    width: 35,
+    height: 35,
+  },
+  header: {
+    fontSize: 11,
+    fontWeight: 'bold',
+  },
+  imageContainer: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    marginBottom: 5,
+  },
+})
